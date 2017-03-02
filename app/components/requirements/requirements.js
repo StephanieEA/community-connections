5871f507fe62805f5fd63179829823beb24a2b8e
Stephanie Andrews <seandrews11@gmail.com>
2017-03-02 01:29:26 -0700

    alter color scheme

--- app/components/requirements/requirements.js
+++ app/components/requirements/requirements.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import Button from '../../components/button/button.js'
+import Input from '../../components/input/input.js'
+
 
 export default class Requirements extends React.Component {
   render() {
@@ -14,31 +16,31 @@ export default class Requirements extends React.Component {
         <Button text='PREVIOUS' className='previous' onClick={this.props.lastStep}/>
         <label>Do you have specific requirements for tenants:</label>
         <br/>
-        <input type='checkbox'
+        <Input type='checkbox'
                name='noSleepovers'
                onChange={this.props.handleInput}/>
                No overnight guests
         <br/>
-        <input type='checkbox'
+        <Input type='checkbox'
                name='noSmoking'
                onChange={this.props.handleInput}/>
                No smoking
         <br/>
-        <input type='checkbox'
+        <Input type='checkbox'
                name='yesClean'
                onChange={this.props.handleInput}/>
                Keep Common Areas Clean
         <br/>
         <section>{inputRequirements}</section>
-        <input placeholder='no pets'
+        <Input placeholder='no pets'
                name='userRequirement'
                value={this.props.userRequirement}
                onChange={this.props.handleInput}/>
-        <button onClick={this.props.addItem}
+        <Button onClick={this.props.addItem}
                 name='requirementTopics'
                 className='userRequirement add-requirements-submit addition'>
                 +
-        </button>
+        </Button>
         <br/>
         <Button text='SUBMIT' className='next' onClick={this.props.nextStep}/>
       </section>
