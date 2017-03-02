fffce1393030fe6b03cc0bd81dd7e01c6bf87818
Stephanie Andrews <seandrews11@gmail.com>
2017-03-02 10:07:06 -0700

    add tests for app

--- app/components/App/App.spec.js
+++ app/components/App/App.spec.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 
 import App from './App';
 
 describe('App', () => {
   it('renders as a <div>', () => {
     const wrapper = shallow(<App/>)
-    assert.equal(wrapper.type(), 'div');
+    console.log(wrapper.debug())
+    assert.equal(wrapper.type(), 'div')
+  });
+
+  it('renders a child header Component', () => {
+    const wrapper = shallow(<App/>)
+    wrapper.debug()
+    expect(wrapper.find('Header')).to.have.length(1);
+  });
+
+  it('renders a child SeniorForm component', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find('SeniorForm')).to.have.length(1);
   });
 });
